277629fb739d484d58caea1e0e619cc706a384c3
DanielGoncalves666 <danielgoncalveskd@gmail.com>
2025-10-09 20:39:00 -0300

    Changes to tcc_experimentos_fogo.sh

--- tcc_experimentos_fogo.sh
+++ tcc_experimentos_fogo.sh
@@ -19,7 +19,7 @@ mkdir -p output/$dir_name
 
 for ambient in "daniel_anfiteatro_fogo_centro" "daniel_anfiteatro_fogo_superior" "daniel_anfiteatro_fogo_porta_esquerda" "daniel_anfiteatro_fogo_duas_portas"; do
     print_in_color "\033[0;34m" "Fire Experiment - $ambient"
-    $RUN_COMMAND -o$dir_name/fire_experiment_$ambient.txt -m4 -e$ambient.txt -O1 -s1 --static-field=5 --kf=10 --ks=2 --kd=1 --delta=0.3 --alpha=0.3 --ignore-self-trace  --skip-new-particles-decay --allow-diagonal-movements --cooldown=5 --traversability=0.7 --print-sff > teste_$ambient.txt
+    $RUN_COMMAND -o$dir_name/fire_experiment_$ambient.txt -m4 -e$ambient.txt -O1 -s1 --static-field=5 --kf=500 --ks=2 --kd=1 --delta=0.3 --alpha=0.3 --ignore-self-trace  --skip-new-particles-decay --allow-diagonal-movements --cooldown=5 --traversability=0.7
 done
 
 
